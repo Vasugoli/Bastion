ad9ec7ee8fc5153a7b13ac3cddd0ee3ba29a3000
Vasugoli <golivasu7@gmail.com>
2026-08-16 11:59:37 +0530

    Basic app setup

---
+++ server/src/main/java/com/vasu/bastionServer/BastionServerApplication.java
@@ -0,0 +1,13 @@
+package com.vasu.bastionServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BastionServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BastionServerApplication.class, args);
+	}
+
+}
