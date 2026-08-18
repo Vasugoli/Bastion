80a3a6ef250c9378a47bed0705a33ba4ac4ceb63
Vasugoli <golivasu7@gmail.com>
2026-08-18 15:03:54 +0530

    Basic health end point set up

--- server/src/main/java/com/vasu/bastionServer/HomeContorller.java
+++ server/src/main/java/com/vasu/bastionServer/HomeContorller.java
@@ -1,12 +1,18 @@
 package com.vasu.bastionServer;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+
+@RestController
 public class HomeContorller {
-	@RequestMapping("/")
-	public String name() {
-		return "index.html";
+	
+	@Value("${spring.mode}")
+	private String DevolopmentMode;
+	
+	@GetMapping("/health")
+	public String health() {
+		return DevolopmentMode;
 	}
 }
